992eaea47572db1d4c4c1fb819c9a64b9f82c646
zhuchenchen <zhuchenchen@std.uestc.edu.cn>
2020-01-16 11:32:53 -0800

    1.16

--- 二维数组中的查找.js
+++ 二维数组中的查找.js
@@ -1,19 +1,16 @@
 function Find(target, array)
 {
     // write code here
-    var leni  = array.length;
-    var lenj = array[0].length;
-    if(leni==0&&lenj==0)
-        return false;
-    var i = 0;
-    var j = lenj-1;
-    while(i<leni &&j>=0){
-        if(target<array[i][j]){
-            j--;
-        }else if(target>array[i][j]){
-            i++;
-        }else
+    var row = 0;
+    var col = array[0].length-1;
+    while(row<=array.length-1&&col>=0){
+        if(target<array[row][col]){
+            col--;
+        }else if(target>array[row][col]){
+            row++;
+        }else{
             return true;
+        }
     }
     return false;
 }
